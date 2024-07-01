def66c1a89007f8046962dc9f8fb871f2a0447cc
AdityaSrivastavaaa <srivastavaaditya015@gmail.com>
2024-07-01 13:23:31 +0530

    work in progress

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -5,6 +5,30 @@ export const CartContext = createContext();
 
 const CartProvider = ({children}) => {
   const [cart , setCart] = useState([]);
+// item amount state 
+const [itemAmount ,setItemAmount] = useState(0)
+
+// total price state 
+const [total , setTotal] = useState(0);
+
+// update total value 
+useEffect(()=>{
+  const total = cart.reduce((accumulator,currentItem)=>{
+    return accumulator + Math.round((currentItem.price* currentItem.amount)*83)
+  },0);
+  setTotal(total)
+})
+
+// update item amount 
+useEffect(()=>{
+  if(cart){
+    const amount = cart.reduce((accumulator,currenItem)=>{
+      return accumulator + currenItem.amount;
+    },0);
+     setItemAmount(amount)
+  } 
+},[cart])
+
   // add to cart 
   const addToCart = (product , id) =>{
     const newItem = {...product,amount:1}
@@ -41,7 +65,35 @@ const CartProvider = ({children}) => {
   const clearCart = () =>{
     setCart([]);
   }
-  return <CartContext.Provider value={{cart , addToCart , removeFromCart , clearCart}}>{children}</CartContext.Provider>;
+
+  // increase amount 
+  const increaseAmount =(id)=>{
+      const cartItem = cart.find((item)=> item.id === id);
+      addToCart(cartItem,id);
+  }
+
+  // decrease amount 
+
+  const decreaseAmount = (id) =>{
+    const cartItem = cart.find((item)=> {
+      return item.id === id});
+    if(cartItem){
+      const newCart = cart.map(item =>{
+        if(item.id === id){
+          return{...item,amount:cartItem.amount -1}
+        }else{
+          return item;
+        }
+      });
+      setCart(newCart);
+
+    }
+    
+      if(cartItem.amount<2){
+        removeFromCart(id);
+      }
+  }
+  return <CartContext.Provider value={{cart , addToCart , removeFromCart , clearCart , increaseAmount , decreaseAmount ,itemAmount , total}}>{children}</CartContext.Provider>;
 };
 
 export default CartProvider;
