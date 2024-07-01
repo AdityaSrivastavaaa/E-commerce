7db85996bb73cd3b7ef520e14fee16b2774244de
AdityaSrivastavaaa <srivastavaaditya015@gmail.com>
2024-07-01 14:24:00 +0530

    completed whole website

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <footer className='bg-primary py-12'>
+    <div className="container mx-auto">
+      <p className='text-white text-center'>Made with &hearts; by Aditya Srivastava . All rights reserved &copy;.</p>
+    </div>
+  </footer>;
 };
 
 export default Footer;
