eb09545f12452cacf550f23ea5072dd7aa22f824
AdityaSrivastavaaa <srivastavaaditya015@gmail.com>
2024-07-01 12:21:31 +0530

    work in progress

---
+++ src/contexts/CartContext.js
@@ -0,0 +1,47 @@
+import React ,{createContext ,useState,useEffect} from 'react';
+
+
+export const CartContext = createContext();
+
+const CartProvider = ({children}) => {
+  const [cart , setCart] = useState([]);
+  // add to cart 
+  const addToCart = (product , id) =>{
+    const newItem = {...product,amount:1}
+    const cartItem = cart.find((item)=>{
+      return item.id === id ;
+    })
+    if(cartItem){
+      const newCart = [...cart].map((item) =>{
+        if(item.id === id){
+          return {...item ,amount:cartItem.amount + 1 };
+        }
+        else{
+          return item;
+        }
+      })
+      setCart(newCart);
+    }else{
+      setCart([...cart,newItem])
+    }
+    console.log(cart)
+
+    console.log(`${product.title} added to the cart`)
+  }
+
+  // remove from the cart 
+  const removeFromCart = (id) =>{
+    const newCart = cart.filter((item)=>{
+      return item.id !== id;
+    });
+    setCart(newCart)
+  }
+
+  //clear cart 
+  const clearCart = () =>{
+    setCart([]);
+  }
+  return <CartContext.Provider value={{cart , addToCart , removeFromCart , clearCart}}>{children}</CartContext.Provider>;
+};
+
+export default CartProvider;
